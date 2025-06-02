1c7d1f6c322b5149113d64bc42dc79aa70acea5a
SeijiHoshino1 <seijihoshino@outlook.com>
2025-06-02 20:27:34 -0300

    2025-06-02

--- js/login.js
+++ js/login.js
@@ -1,1 +1,23 @@
-// login com validacao
+const entrar = document.getElementById('entrar');
+
+entrar.addEventListener('click',
+    function(){
+        let login = document.getElementById('login').value;
+        let senha = document.getElementById('senha').value;
+
+        //memoria local
+        const usuariosCadastrados = JSON.parse(
+            localStorage.getItem("usuarios")
+        ) || [];
+
+        let usuarioEncontrado = usuariosCadastrados.find(
+            u => u.usuario === login && u.senha === senha // Outra forma de fazer função anônima
+        );
+        if (usuarioEncontrado){
+            localStorage.setItem("userLogado", login);
+            window.location.href = "dashboard.html";
+        }else{
+            document.getElementById("mensagem").textContent = "Login ou senha incorretos";
+        }
+    }
+);
